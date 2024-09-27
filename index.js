1a4edd590a25487c31d880235365cb4eefada3b9
Miguel <galleriesproductions@gmail.com>
2024-09-27 19:35:08 -0400

    set up user input questions for README generator

--- index.js
+++ index.js
@@ -1,7 +1,62 @@
 // TODO: Include packages needed for this application
+import inquirer from 'inquirer';
 
 // TODO: Create an array of questions for user input
-const questions = [];
+
+const questions = [
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Please provide a short description of your project:',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'What are the installation instructions?',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Provide instructions and examples for use:',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'What license will your project use?',
+      choices: [
+        'MIT',
+        'GPLv3',
+        'Apache 2.0',
+        'None'
+      ],
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'What are the contribution guidelines?',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'How do you run tests for this project?',
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'What is your GitHub username?',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is your email address?',
+    }
+  ];
+  
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
